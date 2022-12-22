df1c223679b506585539d0cca057e08bb9401ba1
Vladimir Kharlampidi <harlamp_83@yahoo.com>
2022-12-22 21:55:59 +0300

    yrok 1

--- Task1/task1.py
+++ Task1/task1.py
@@ -2,15 +2,16 @@
 построчно данные, вводимые пользователем. Об окончании ввода данных
 свидетельствует пустая строка."""
 
-my_list = []
-while True:
-    line = input("Enter anything: ")
-    if line == '':
-        print(my_list)
-        exit()
-    else:
-        newline = line + '\n'
-        my_list.append(newline)
+my_f = open('test.txt', 'w')
+line = input('Введите текст \n')
+while line:
+    my_f.writelines(line)
+    line = input('Введите текст \n')
+    if not line:
+        break
 
-    with open("test_1.txt", "w") as file_obj:
-        file_obj.writelines(my_list)
+my_f.close()
+my_f = open('test.txt', 'r')
+content = my_f.readlines()
+print(content)
+my_f.close()
